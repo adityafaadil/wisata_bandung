97ca81f098cadb76e1d284eb1bfba40ab8887b37
fadil-21 <fadiljakarta11@gmail.com>
2022-01-09 10:43:09 +0700

    add splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_bandung/main_screen.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -11,5 +12,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(),
       home: MainScreen(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                SecondScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Image.asset('images/flutter.jpg'),
+      ),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainScreen();
   }
 }
